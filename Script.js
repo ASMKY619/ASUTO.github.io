1d189f55a37c71b017131838a27e0bc37b393e6f
ASMKY619 <akito061919@i.softbank.jp>
2025-03-23 17:01:52 +0900

    Create Script.js

---
+++ Script.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById("reserveButton").addEventListener("click", function() {
+        alert("予約機能は現在準備中です。お電話またはお問い合わせフォームからご連絡ください。");
+    });
+});
